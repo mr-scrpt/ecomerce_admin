789d1eb0c13a62fb716b4815fc9dcc877c6916f1
mr.scrpt <mr.scrpt@gmail.com>
2023-12-26 10:37:25 +0200

    feat: api route delete billboard by id

--- app/api/[storeSlug]/billboards/[billboardId]/route.ts
+++ app/api/[storeSlug]/billboards/[billboardId]/route.ts
@@ -1,7 +1,8 @@
-import { billboardAction } from "@/fsd/entity/Billboard";
+import { IBillboard, billboardAction } from "@/fsd/entity/Billboard";
 import { buildError } from "@/fsd/shared/lib/buildError";
 import { HttpException } from "@/fsd/shared/lib/httpException";
 import { buildResponse } from "@/fsd/shared/lib/responseBuilder";
+import { ResponseDataAction } from "@/fsd/shared/type/response.type";
 import { NextResponse } from "next/server";
 
 interface IMetaBillboard {
@@ -10,13 +11,36 @@ interface IMetaBillboard {
     billboardId: string;
   };
 }
-export const GET = async (_: Request, meta: IMetaBillboard) => {
+export const GET = async (
+  _: Request,
+  meta: IMetaBillboard,
+): Promise<NextResponse<ResponseDataAction<IBillboard | null>>> => {
   try {
     const { params } = meta;
     const { billboardId } = params;
     const { data, error, status } =
       await billboardAction.getBillboard(billboardId);
 
+    if (error) {
+      throw new HttpException(error, status);
+    }
+    const response = buildResponse(data);
+    return NextResponse.json(response);
+  } catch (e) {
+    const { error, status } = buildError(e);
+    const errorResponse = buildResponse(null, error, status);
+    return NextResponse.json(errorResponse, { status });
+  }
+};
+
+export const DELETE = async (_: Request, meta: IMetaBillboard) => {
+  console.log(" =>>> in delete");
+  try {
+    const { params } = meta;
+    const { billboardId } = params;
+
+    const { data, error, status } =
+      await billboardAction.removeBillboard(billboardId);
     if (error) {
       throw new HttpException(error, status);
     }
